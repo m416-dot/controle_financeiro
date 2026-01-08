6ffcd0946cf7a2d8559acd4f319575e0561a5669
m416-dot <mvitorio416@gmail.com>
2026-01-08 13:43:33 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const path = require("path");
 // CONFIGURAÇÕES
 // ============================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ============================
 // MIDDLEWARES
